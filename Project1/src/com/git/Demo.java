3366ab4b195d1348311eb2a17ab6df5f8f69e3bd
SagnikRahman <121563860+SagnikRahman@users.noreply.github.com>
2026-03-09 12:08:17 +0530

    Now a = 20

--- Project1/src/com/git/Demo.java
+++ Project1/src/com/git/Demo.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Demo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = 10;
+		int a = 20;
 		int b = 20;
 		
 		System.out.println(a+b);
